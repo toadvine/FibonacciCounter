d55645b9594211e8b8a9f7cd0dd7cd83ba9a4ca3
toadvine <drubbles@gmail.com>
2015-09-02 11:52:06 -0700

    learning how to count indices

--- fCounter.py
+++ fCounter.py
@@ -1,18 +1,20 @@
 __author__ = 'benjamin_sanchez'
 import time  # needed for timing
+# This script is for confirming this worked and performance isn't a concern
 
 start = time.time()  # timing
 print("We're going to calculate 1000 Fibonacci numbers now...")
 a = 0
 b = 1
-answer = list()
-answer.append(a)
-answer.append(b)
+fList = list()
+fList.append(a)
+fList.append(b)
 for x in range(1000):
     a += b
-    answer.append(a)
+    fList.append(a)
     b += a
-    answer.append(b)
-end = time.time()  #t iming
+    fList.append(b)
+end = time.time()  # timing
 print("It took this long to calculate 1000 iterations: ", (end - start))
-print(answer)
+print(fList)
+
---
+++ fPlacer.py
@@ -0,0 +1,32 @@
+__author__ = 'benjamin_sanchez'
+# Cleaned up and expanded for code abbey
+
+
+def main():
+    aCount = int(input())
+    fList = fibo(1000)
+    answer = []
+    for x in range(aCount):
+        bNum = int(input())
+        count = 0
+        for x in fList:
+            if x == bNum:
+                answer.append(count)
+            count += 1
+
+
+def fibo(count):
+    a = 0
+    b = 1
+    fList = list()
+    fList.append(a)
+    fList.append(b)
+    for x in range(count):
+        a += b
+        fList.append(a)
+        b += a
+        fList.append(b)
+    return fList
+
+
+main()
