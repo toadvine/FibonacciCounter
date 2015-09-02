b326d97c3ef53eaedd6f6edf807ab9eae2aebbac
toadvine <drubbles@gmail.com>
2015-09-02 11:21:17 -0700

    exploring lists and speed

--- fCounter.py
+++ fCounter.py
@@ -2,10 +2,10 @@ __author__ = 'benjamin_sanchez'
 import time
 
 start = time.time()
-print ("We're going to calculate 1000 Fibonacci numbers now...")
+print("We're going to calculate 1000 Fibonacci numbers now...")
 a = 0
 b = 1
-answer = []
+answer = list()
 answer.append(a)
 answer.append(b)
 for x in range(1000):
@@ -14,5 +14,5 @@ for x in range(1000):
     b += a
     answer.append(b)
 end = time.time()
-print ("It took this long to calculate 1000 iterations: ", (end - start))
-print (answer)
+print("It took this long to calculate 1000 iterations: ", (end - start))
+print(answer)
