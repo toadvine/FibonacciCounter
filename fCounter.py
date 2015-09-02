70a125635118d95a9ba9a60a7ceb8aeec5c3938e
toadvine <drubbles@gmail.com>
2015-09-02 11:18:13 -0700

    exploring lists and speed

---
+++ fCounter.py
@@ -0,0 +1,18 @@
+__author__ = 'benjamin_sanchez'
+import time
+
+start = time.time()
+print ("We're going to calculate 1000 Fibonacci numbers now...")
+a = 0
+b = 1
+answer = []
+answer.append(a)
+answer.append(b)
+for x in range(1000):
+    a += b
+    answer.append(a)
+    b += a
+    answer.append(b)
+end = time.time()
+print ("It took this long to calculate 1000 iterations: ", (end - start))
+print (answer)
